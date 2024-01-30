84a715c9a8ec94054cd95d4709666dfe56883880
Amit Kumar <amitkr4538@gmail.com>
2024-01-30 22:24:17 +0530

    update the card ui and image

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,13 +1,12 @@
-import asteriod_img from "../assets/Asteroid_Transparent.png";
-import styles from "./Cards.module.css";
+import styles from './Cards.module.css';
 
 function Card() {
   return (
     <div className={styles.card}>
       <img
         className={styles.card_img}
-        src={asteriod_img}
-        alt="Asteroid image"
+        src="https://via.placeholder.com/150"
+        alt="placeholder image"
       ></img>
       <hr></hr>
       <h2 className={styles.card_title}>Asteriod Material</h2>
